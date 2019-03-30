d2e0ab0e60bf9179200b52eb542c2b913dbce1c0
mykhailo-riabokon <mykhailo.riabokon@gmail.com>
2019-03-30 18:55:53 +0200

    base styles

--- src/layouts/BaseLayout/Header/index.js
+++ src/layouts/BaseLayout/Header/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 function Header() {
+  const name = "Shamly user name";
+
   return (
     <div className="header">
       <div className="logo">
         <div className="icon" />
         <div className="text">shalmy</div>
       </div>
+      {name && <div className="user-name">{name}</div>}
     </div>
   );
 }
