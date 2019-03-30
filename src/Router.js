ab2571b26125ae56c5fa04f962048ae0945cedaf
mykhailo-riabokon <mykhailo.riabokon@gmail.com>
2019-03-30 16:35:19 +0200

    add screen

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Recommendations from "./pages/Recommendations";
 import Backoffice from "./pages/Backoffice";
+import Onboarding from "./pages/Onboarding";
 
 class Router extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class Router extends React.Component {
         <Switch>
           <Route path="/recommendations" component={Recommendations} />
           <Route path="/backoffice" component={Backoffice} />
+          <Route path="/" component={Onboarding} />
         </Switch>
       </BrowserRouter>
     );
---
+++ src/pages/Onboarding/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Onboarding extends React.Component {
+  render() {
+    return <div>Place to add steps</div>;
+  }
+}
+
+export default Onboarding;
