701668c47b0fbe2ae976a895e677fab93cde61ac
mykhailo-riabokon <mykhailo.riabokon@gmail.com>
2019-03-30 20:53:32 +0200

    location

--- src/pages/Onboarding/Steps/ExerciseLocation/index.js
+++ src/pages/Onboarding/Steps/ExerciseLocation/index.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Form, Input, Button } from "antd";
 
-function ExerciseLocation() {
-  return "ExerciseLocation";
+class ExerciseLocation extends React.Component {
+  validate = () => {
+    return new Promise(resolve => {
+      this.props.form.validateFields((errors, values) => {
+        resolve({ errors, values });
+      });
+    });
+  };
+
+  onSubmit = async () => {
+    const { errors, values } = await this.validate();
+
+    if (!errors) {
+      this.props.onSubmit(values);
+    }
+  };
+
+  render() {
+    return (
+      <Form>
+        <Form.Item label="Location">
+          {this.props.form.getFieldDecorator("location", {
+            rules: [{ required: true }]
+          })(<Input placeholder="location" />)}
+          <Button type="primary" onClick={this.onSubmit}>
+            Next
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
 }
 
-export default ExerciseLocation;
+export default Form.create()(ExerciseLocation);
