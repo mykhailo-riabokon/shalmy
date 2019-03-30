49b3f676fa7dacfc28296d173f8f4809266c9d4b
mykhailo-riabokon <mykhailo.riabokon@gmail.com>
2019-03-30 20:43:41 +0200

    place

--- src/pages/Onboarding/Steps/ExercisePlace/index.js
+++ src/pages/Onboarding/Steps/ExercisePlace/index.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { Form, Radio, Button } from "antd";
 
-function ExercisePlace() {
-  return "ExercisePlace";
+class ExercisePlace extends React.Component {
+  validate = () => {
+    return new Promise(resolve => {
+      this.props.form.validateFields((errors, values) => {
+        resolve({ errors, values });
+      });
+    });
+  };
+
+  onSubmit = async () => {
+    const { errors, values } = await this.validate();
+
+    if (!errors) {
+      this.props.onSubmit(values);
+    }
+  };
+
+  render() {
+    return (
+      <Form>
+        <Form.Item label="Place">
+          {this.props.form.getFieldDecorator("reason", {
+            rules: [{ required: true }]
+          })(
+            <Radio.Group>
+              <Radio value={1}>A</Radio>
+              <Radio value={2}>B</Radio>
+              <Radio value={3}>C</Radio>
+              <Radio value={4}>D</Radio>
+            </Radio.Group>
+          )}
+          <Button type="primary" onClick={this.onSubmit}>
+            Next
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
 }
 
-export default ExercisePlace;
+export default Form.create()(ExercisePlace);
