595abe5aef39a43a610e5fbd566b02a7b52ef4cc
mykhailo-riabokon <mykhailo.riabokon@gmail.com>
2019-03-31 11:52:07 +0300

    logo

--- src/layouts/BaseLayout/index.js
+++ src/layouts/BaseLayout/index.js
@@ -8,6 +8,26 @@ function BaseLayout(props) {
     <div className="base-layout">
       <Header />
       <div className="content">{props.children}</div>
+      <footer>
+        <div>
+          Logo made by{" "}
+          <a href="https://www.freepik.com/" title="Freepik">
+            Freepik
+          </a>{" "}
+          from{" "}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>{" "}
+          is licensed by{" "}
+          <a
+            href="http://creativecommons.org/licenses/by/3.0/"
+            title="Creative Commons BY 3.0"
+            target="_blank"
+          >
+            CC 3.0 BY
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
