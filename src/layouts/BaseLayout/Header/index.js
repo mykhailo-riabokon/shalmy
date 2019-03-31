91021b9012190968952a932f4d70fb1ef339f462
mykhailo-riabokon <mykhailo.riabokon@gmail.com>
2019-03-31 10:08:36 +0300

    feeback

--- src/layouts/BaseLayout/Header/index.js
+++ src/layouts/BaseLayout/Header/index.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Modal, Input } from "antd";
 import { UserDataContext } from "../../../context";
 
 class Header extends React.Component {
   static contextType = UserDataContext;
 
+  state = {
+    isOpen: false
+  };
+
   render() {
     const { name } = this.context.userData;
+    const close = () => this.setState({ isOpen: false });
 
     return (
       <div className="header">
@@ -13,7 +19,24 @@ class Header extends React.Component {
           <div className="icon" />
           <div className="text">shalmy</div>
         </div>
-        {name && <div className="user-name">{name}</div>}
+        {name && (
+          <div
+            className="feedback"
+            onClick={() => this.setState({ isOpen: true })}
+          >
+            Leave your feeback
+          </div>
+        )}
+        <Modal
+          width={500}
+          visible={this.state.isOpen}
+          closable={false}
+          onOk={close}
+          onCancel={close}
+        >
+          <h3>Your feedback</h3>
+          <Input.TextArea />
+        </Modal>
       </div>
     );
   }
